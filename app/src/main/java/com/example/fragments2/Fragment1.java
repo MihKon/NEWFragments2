03c7b73ba548bfda30a45948907aa45382a1c1e8
Tapok <Tapok>
2020-02-21 13:15:58 +0300

    это другое задание

---
+++ app/src/main/java/com/example/fragments2/Fragment1.java
@@ -0,0 +1,41 @@
+package com.example.fragments2;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Fragment1 extends Fragment {
+
+    //LOGIN
+
+    private View view;
+    public Fragment1() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_fragment1, container, false);
+        TextInputLayout textInputLayout = view.findViewById(R.id.enter_login);
+        TextInputEditText
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+}
